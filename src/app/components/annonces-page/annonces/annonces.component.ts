2456a593dc617a0884e2f93bbfe8ec58f0471ba2
Yahya Boujrah <boujrah.yahya@gmail.com>
2023-05-03 18:17:50 +0100

    retouches

--- src/app/components/annonces-page/annonces/annonces.component.ts
+++ src/app/components/annonces-page/annonces/annonces.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AnnonceService } from 'src/app/services/Annonce.service';
 import { faNewspaper} from '@fortawesome/free-solid-svg-icons';
 import { CustomResponse } from 'src/app/interfaces/Custom-response';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-annonces',
@@ -19,6 +20,9 @@ export class AnnoncesComponent implements OnInit {
   ngOnInit(): void {
     this.annonceService.annonces$.subscribe(response =>{
       this.annonceResponse = { ...response , data: { annonces: response.data.annonces?.reverse() } }
+    },
+    (error : HttpErrorResponse) => {
+      alert(error.message)
     });
 
   }
