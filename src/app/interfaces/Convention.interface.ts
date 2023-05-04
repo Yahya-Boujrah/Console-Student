432ccf3a45563f3e237a977a5e8e059f70355eb2
Yahya Boujrah <boujrah.yahya@gmail.com>
2023-05-04 04:07:26 +0100

    convention service + interface + retouches navbar

---
+++ src/app/interfaces/Convention.interface.ts
@@ -0,0 +1,19 @@
+export interface Convention {
+    id?: number;
+    Nom?: string;
+    nomSociete : string;
+    adresseSociete : string;
+    EmailSociete : string;
+    NomRepresentantSociete : string;
+    NomRepresentantEcole : string;
+    sujetStage : string;
+    dateDebutStage : Date;
+    dateFinStage : Date;
+    nomEncadrantSociete : string;
+    nomEncadrantEcole : string;
+    numeroContrantAssurance : string;
+    montantGratification : number;
+    modalitePaiementGratification : string;
+
+
+}
---
+++ src/app/services/Convention.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from "@angular/core";
+import{HttpClient, HttpHeaders} from "@angular/common/http"
+
+import { Observable, catchError, of, tap } from "rxjs";
+import { CustomResponse } from "../interfaces/Custom-response";
+import { Convention } from "../interfaces/Convention.interface";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable()
+export class ConventionService {
+  private readonly apiUrl = 'http://localhost:8080';  
+
+  constructor(private http: HttpClient) {}
+
+  conventions$ = <Observable<CustomResponse>>
+    this.http.get<CustomResponse>(`${this.apiUrl}/conventions/list`)
+    .pipe(
+      tap(console.log),
+      catchError(() => {
+        return of('error')
+      })
+    );
+
+  saveConvention$ = (convention:Convention) => <Observable<CustomResponse>>
+  this.http.post<CustomResponse>(`${this.apiUrl}/conventions/save` , convention, httpOptions)
+  .pipe(
+    tap(console.log),
+    catchError(() => {
+      return of('error')
+    })
+  );
+
+  updateConvention$ = (convention:Convention) => <Observable<CustomResponse>>
+  this.http.post<CustomResponse>(`${this.apiUrl}/conventions/update` , convention, httpOptions)
+  .pipe(
+    tap(console.log),
+    catchError(() => {
+      return of('error')
+    })
+  );
+  convention$ =  (conventionId: number) => <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.apiUrl}/conventions/get/${conventionId}`)
+  .pipe(
+    tap(console.log),
+    catchError(() => {
+      return of('error')
+    })
+  );
+
+  deleteConvention$ = (conventionId: number) => <Observable<CustomResponse>>
+  this.http.delete<CustomResponse>(`${this.apiUrl}/conventions/delete/${conventionId}`)
+  .pipe(
+    tap(console.log),
+    catchError(() => {
+      return of('error')
+    })
+  );
+}
